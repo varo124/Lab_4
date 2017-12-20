af959f9fcdeaca3ab12151c05cbb5ddc4a3cdd02
varo124 <34127710+varo124@users.noreply.github.com>
2017-12-21 02:57:53 +0300

    Update kod2.cpp

--- kod2.cpp
+++ kod2.cpp
@@ -1,17 +1,4 @@
-switch (driver.Status) 
+m_driverStatusNames getDriver status(driver.Status) 
 { 
-case ClientStatus.Unknown: 
-return m_driverStatusNames[ClientStatus.Unknown]; 
-case ClientStatus.Free: 
-return m_driverStatusNames[ClientStatus.Free]; 
-case ClientStatus.Busy: 
-return m_driverStatusNames[ClientStatus.Busy]; 
-case ClientStatus.InWay: 
-return m_driverStatusNames[ClientStatus.InWay]; 
-case ClientStatus.Work: 
-return m_driverStatusNames[ClientStatus.Work]; 
-case ClientStatus.Break: 
-return m_driverStatusNames[ClientStatus.Break]; 
-case ClientStatus.Alarm: 
-return m_driverStatusNames[ClientStatus.Alarm]; 
+return m_driverStatusNames[driver.Status]; 
 } 
