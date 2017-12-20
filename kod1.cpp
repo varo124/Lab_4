3e43b4a5fb8e1b19bc02e0f338397c4241b28aab
varo124 <34127710+varo124@users.noreply.github.com>
2017-12-21 02:56:53 +0300

    Update kod1.cpp

--- kod1.cpp
+++ kod1.cpp
@@ -1,19 +1,8 @@
 private void set_mode(bool mod) 
 { 
-if (mod == true) 
-{ 
-label1.Enabled = true; 
-button1.Enabled = true; 
-button2.Enabled = true; 
-button3.Enabled = false; 
-button4.Enabled = false; 
-} 
-else 
-{ 
-label1.Enabled = false; 
-button1.Enabled = false; 
-button2.Enabled = false; 
-button3.Enabled = true; 
-button4.Enabled = true; 
-} 
+label1.Enabled = mod; 
+button1.Enabled = mod; 
+button2.Enabled = mod; 
+button3.Enabled = !mod; 
+button4.Enabled = !mod; 
 } 
