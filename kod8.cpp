991f6412388eee975e691b0ea37ee9fa029beb66
varo124 <34127710+varo124@users.noreply.github.com>
2017-12-21 03:06:23 +0300

    Update kod8.cpp

--- kod8.cpp
+++ kod8.cpp
@@ -1,5 +1,2 @@
-DateTime dt = DateTime.Now; 
-string h = dt.Hour.ToString().PadLeft(2, '0'); 
-string m = dt.Minute.ToString().PadLeft(2, '0'); 
-string s = dt.Second.ToString().PadLeft(2, '0'); 
-Console.WriteLine("--"+h+":"+m+":"+s+"--"); 
+CultureInfo ci = new CultureInfo("de-DE"); 
+Console.WriteLine(dt.ToString("d", ci)); 
