55d7ee1d8d3598803f857751e501a5ba780c9fc2
varo124 <34127710+varo124@users.noreply.github.com>
2017-12-21 03:07:00 +0300

    Update kod9.cpp

--- kod9.cpp
+++ kod9.cpp
@@ -1,1 +1,3 @@
-return ((int)(Counter / 2) != Counter / 2.00 && Counter != 0);
+int FriendlyVariableName1 =Counter / 2; 
+int FriendlyVariableName2 = Counter / 2.00; 
+return (FriendlyVariableName1 != FriendlyVariableName2 && Counter !=0);
