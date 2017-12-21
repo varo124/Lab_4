7450bccc87423e17cc9e793d84676f9b0b531450
varo124 <34127710+varo124@users.noreply.github.com>
2017-12-21 03:04:12 +0300

    Update kod6.cpp

--- kod6.cpp
+++ kod6.cpp
@@ -3,12 +3,11 @@ foreach (DirectoryInfo dir in dirs.GetDirectories())
 //create folder{16} 
 stream.Write(new byte[] { (byte) NetworkMessage.MakeDir }, 0, 1); 
 stream.Read(new byte[1],0, 1); 
-stream.Write(BitConverter.GetBytes(Encoding.UTF8.GetBytes( 
-SubFolder.Replace('\\', '/') + dir.Name.Replace('\\', '/')).Length), 0, 4); 
-stream.Write(Encoding.UTF8.GetBytes(SubFolder.Replace('\\', '/') + 
-dir.Name.Replace('\\', '/')),0, 
-Encoding.UTF8.GetBytes(SubFolder.Replace('\\', '/') + 
-dir.Name.Replace('\\', '/')).Length); 
+stream.Write(BitConverter.GetBytes(Encoding.UTF8.GetBytes((a + b.Length), 0, 4); 
+stream.Write(Encoding.UTF8.GetBytes (a + b,0, Encoding.UTF8.GetBytes 
+(a + b.Length); 
 //send folder name 
-stream.Read(new byte[1], 0, 1);//OK 
+stream.Read(new byte[1], 0, 1);//Ok 
+vat a = SubFolder.Replace('\\', '/'); 
+var b = dir.Name.Replace('\\', '/')); 
 } 
