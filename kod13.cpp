0451d8e367ecf6d603d71fb4011bb57cc7b89949
varo124 <34127710+varo124@users.noreply.github.com>
2017-12-21 03:10:46 +0300

    Update kod13.cpp

--- kod13.cpp
+++ kod13.cpp
@@ -1,4 +1,3 @@
-//Use the Name field 
 string[] nameParts = customer.Name.Split(' '); 
 string firstName = nameParts[0]; 
-string lastName = customer.Name.Replace(nameParts[0], "").TrimStart(' '); 
+string lastName = nameParts[1].TrimStart(' '); 
