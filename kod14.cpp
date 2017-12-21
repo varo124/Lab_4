e8f7d1e4bb3fbeb14c60d3d2a7a26bfef12a7473
varo124 <34127710+varo124@users.noreply.github.com>
2017-12-21 03:12:26 +0300

    Add 14 files

---
+++ kod14.cpp
@@ -0,0 +1,4 @@
+foreach (string id in sourceIDs.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))             
+{	sourceId = Convert.ToInt32(id);               
+  	break;   
+}          
