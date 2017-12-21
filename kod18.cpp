1b0c54ec688ce30daded56da14f1de6d43df31ef
varo124 <34127710+varo124@users.noreply.github.com>
2017-12-21 03:15:39 +0300

    Update kod18.cpp

--- kod18.cpp
+++ kod18.cpp
@@ -1,4 +1,3 @@
-private static readonly char SPECIFIER = "$"[0]; 
-private static readonly char DELIMITER = ":"[0]; 
-private static readonly char[] DELIMITER_ARRAY = new char[1] { DELIMITER }; 
-
+private static readonly string SPECIFIER = "$"; 
+private static readonly string DELIMITER = ":"; 
+private static readonly string[] DELIMITER_ARRAY = new string[1] { DELIMITER }; 
